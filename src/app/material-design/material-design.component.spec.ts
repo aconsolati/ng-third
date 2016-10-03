5eca561f7c2436ded66cde5199e18a7064299e93
aconsolati <aconsolati@gmail.com>
2016-10-03 13:46:19 +1000

    Add components to be used in routing

---
+++ src/app/material-design/material-design.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MaterialDesignComponent } from './material-design.component';
+
+describe('Component: MaterialDesign', () => {
+  it('should create an instance', () => {
+    let component = new MaterialDesignComponent();
+    expect(component).toBeTruthy();
+  });
+});
