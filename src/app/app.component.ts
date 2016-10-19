a2660ca8b454136d54e8e1a88a393a1367a5015b
aconsolati <aconsolati@gmail.com>
2016-10-19 20:24:45 +1000

    Theming support - light/dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  isDarkTheme = false;
+  
   views: Object[] = [
     {
       name: "Appointments",
